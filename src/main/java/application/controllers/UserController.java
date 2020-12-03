53df6cc27eb32f16e2102f471716a6d7b8d68f51
Hafuru <75428571+Hafuru@users.noreply.github.com>
2020-12-03 21:49:47 +0100

    ;

--- src/main/java/application/controllers/UserController.java
+++ src/main/java/application/controllers/UserController.java
@@ -77,7 +77,7 @@ public class UserController
     {
         if (userRepository.existsById(id))
         {
-          System.out.println("ee")
+          System.out.println("ee");
             return userRepository.findById(id).get();
         }
 
